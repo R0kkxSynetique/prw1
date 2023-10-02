06c25d1f05ffc62253e4d222ef65fd751c82a6f9
Benjamin Fontana <ben192003@gmail.com>
2023-10-02 12:20:00 +0200

    added upload file view

---
+++ UploadFichier/src/views/images/uploadImages.php
@@ -0,0 +1,27 @@
+<?php $title = "Upload" ?>
+<div class="col-md-4 col-md-offset-4">
+    <form class="form-horizontal" method="post" enctype="multipart/form-data">
+        <h1 class="text-center">Upload</h1>
+        <div class="form-group">
+            <label for="categories" class="control-label">Catégories</label>
+            <select id="categories" name="categories" required>
+                <?php foreach ($data['categories'] as $category) : ?>
+                    <option value="<?= $category ?>"><?= $category ?></option>
+                <?php endforeach ?>
+            </select>
+            <?php if ($data['category_error'] ?? null) : ?>
+                <span class="help-block"><?= $data['category_error'] ?></span>
+            <?php endif; ?>
+        </div>
+        <div>
+            <input type="file" name="image" accept="image/*" required>
+        </div>
+        <?php if ($data['image_error'] ?? null) : ?>
+            <span class="help-block"><?= $data['image_error'] ?></span>
+        <?php endif; ?>
+        <br>
+        <div class="form-group">
+            <button type="submit" class="btn btn-lg btn-success btn-block">Envoyer</button>
+        </div>
+    </form>
+</div>
