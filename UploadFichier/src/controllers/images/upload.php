244300675675959dcf9b45b3ce8fdddc97d514e7
Benjamin Fontana <ben192003@gmail.com>
2023-10-06 14:25:58 +0200

    updated the image controller to check the mime types
    now only accept jpeg, png, gif

--- UploadFichier/src/controllers/images/upload.php
+++ UploadFichier/src/controllers/images/upload.php
@@ -5,7 +5,16 @@ require_once SOURCE_DIR . '/models/image.php';
 $bag['data'] = [];
 
 $category = trim($_POST['categories']);
-$image    = $_FILES['image'];
+$image = $_FILES['image'];
+$fileinfo = finfo_open(FILEINFO_MIME_TYPE);
+$filename = $image['tmp_name'];
+
+$IMAGE_MIME_TYPES = [
+    'image/jpeg',
+    'image/png',
+    'image/gif'
+];
+
 
 if (!in_array($category, getAllCategories())) {
     $bag['data']['category_error'] = "La catégorie n'existe pas";
@@ -15,14 +24,19 @@ if ($image['error'] != UPLOAD_ERR_OK) {
     $bag['data']['image_error'] = "Une erreur est survenue lors de l'upload de l'image";
 }
 
+if (!in_array(finfo_file($fileinfo, $filename), $IMAGE_MIME_TYPES)){
+    $bag['data']['image_error'] = "Le fichier doit être une image";
+}
+
 if (empty($bag['data'])) {
+
     uploadImage($category, $bag['current_user']['name'], $image);
 
     $bag['headers'] = ['Location' => '/images'];
     return $bag;
 }
 
-$bag['data'] = ['categories' => getAllCategories()];
+$bag['data']['categories'] = getAllCategories();
 $bag['view'] = 'views/images/uploadImages';
 
 return $bag;
